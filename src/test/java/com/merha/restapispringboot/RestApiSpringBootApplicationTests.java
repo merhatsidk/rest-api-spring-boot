93760d798d8359f5da5a56df35d2b688fccfe426
merhatsidk <merhatsidk29@gmail.com>
2023-02-15 09:48:22 -0600

    initial commit

---
+++ src/test/java/com/merha/restapispringboot/RestApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merha.restapispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
