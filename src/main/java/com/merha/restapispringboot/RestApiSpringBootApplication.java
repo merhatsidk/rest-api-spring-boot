beb8417a9a719d58f62bea26269372943a15927f
merhatsidk <merhatsidk29@gmail.com>
2023-02-15 10:46:01 -0600

    pom file updated git add .

--- src/main/java/com/merha/restapispringboot/RestApiSpringBootApplication.java
+++ src/main/java/com/merha/restapispringboot/RestApiSpringBootApplication.java
@@ -2,12 +2,20 @@ package com.merha.restapispringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RestApiSpringBootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RestApiSpringBootApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello(){
+        return "welcome !!";
     }
 
 }
